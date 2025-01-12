5bb757bdbfcecc425e61d01ba3ae66ce5f042619
Jina Kim <drjina02@gmail.com>
2025-01-12 09:24:36 +0900

    minor

--- beep_consultant/src/api/chat_api.tsx
+++ beep_consultant/src/api/chat_api.tsx
@@ -23,7 +23,6 @@ export const get_person_info = async (name: string): Promise<any> => {
 export const create_chat_room = async (createdata: CreateMentorChatRequest) => {
   try {
     const response = await axiosInstance.post('/chat-rooms', createdata);
-    console.log(response);
     return response;
   } catch (error) {
     console.error('멘토 채팅방 생성 실패:', error);
@@ -33,7 +32,6 @@ export const create_chat_room = async (createdata: CreateMentorChatRequest) => {
 
 export const start_chat = async (query: StartChatRequest, room_id: string, user_id: string) => {
   try {
-    console.log(query, room_id, user_id);
     const response = await axiosInstance2.post(`/chat-rooms/${room_id}/messages/?user_id=${user_id}`, query);
     return response;
   } catch (error) {
